03d26c9bc578e68c25cd1762d2c0f72e1d369d58
wcorn <dsk08208@gmail.com>
2022-01-29 22:36:29 +0900

    java_chess

--- Chapter1/src/Test/Test3.java
+++ Chapter1/src/Test/Test3.java
@@ -3,28 +3,24 @@ import java.util.*;
 public class Test3 {
 
 	public static void main(String[] args) {
-		        Scanner sc = new Scanner(System.in);
-		        String str = sc.nextLine();
-		        String[] str2 = str.split(" ");
-		        int in1 = Integer.parseInt(str2[0]);
-		        int in2 = Integer.parseInt(str2[1]);
-		        if(in2==1) {
-		        	System.out.println(in1);
-		        }
-		        else if(in2==0) {
-		        	System.out.println(0);
-		        }
-		        else{
-		        	System.out.println(cycle(in1-1,in2-1));
-		        }
-		        sc.close();
-		    }
-		    public static int cycle(int n1, int n2){
-		        	if(n2>1&&n1>=n2){
-		        		return cycle(n1-1,n2);
-		        	}
-		    }
+		Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str2[] = str.split(" ");
+        int in1 = Integer.parseInt(str2[0]);
+        int in2 = Integer.parseInt(str2[1]);
+        char[][] arr = new char[in1][in2];
+        char[][] by = new char[8][8];
+        for(int i=0; i<in1;i++){
+            String str3 = sc.nextLine();
+            char[] bi = str3.toCharArray();
+            for(int j=0; j<str3.length();j++) {
+            	arr[i][j] = bi[j];
+            }
+        }    
+        
 	}
+	
+}
 
 
 
