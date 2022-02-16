b23a2c758be18a1e32173b3ebf9b00fa25c9db7f
Kang-DS <dsk08208@gmail.com>
2022-02-17 01:12:00 +0900

    baekjoon

--- baekjoon_/src/baekjoon_1/baekjoon_10757.java
+++ baekjoon_/src/baekjoon_1/baekjoon_10757.java
@@ -12,23 +12,33 @@ public class baekjoon_10757{
         int[] n1 = new int[10001];
         int[] n2 = new int[10001];
         int[] result = new int[10002];
-        int max=Integer.max(ch1.length, ch1.length);
+        int max=Integer.max(ch1.length, ch2.length);
         for(int i=0;i<ch1.length;i++) {
         	n1[i] = (int)(ch1[ch1.length-1-i])-48;
-        	sb.append(n1[i]+"\n");
         }
         for(int i=0;i<ch2.length;i++) {
         	n2[i] = (int)(ch2[ch2.length-1-i])-48;
-        	sb.append(n2[i]+"\n");
         }
-        for(int i=0;i<max+1;i++) {
+        for(int i=0;i<max;i++) {
         	if(n1[i]+n2[i]>=10) {
-        		result[i]=n1[i]+n2[i]-10;
+        		result[i]+=(n1[i]+n2[i]-10);
         		result[i+1]++;
         	}
         	else {
-        		result[i]=n1[i]+n2[i];
+        		result[i]+=(n1[i]+n2[i]);
         	}
+        	if(result[i]>=10) {
+        		result[i]-=10;
+        		result[i+1]++;
+        	}
+        }
+        if(result[max]==0) {
+        	max--;
+        }
+        if(result[max+1]!=0) {
+        	max++;
+        }
+        for(int i=max;i>=0;i--) {
         	sb.append(result[i]);
         }
         bw.write(sb.toString());
