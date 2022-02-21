be5d743d91b89408e96f801efff6adf7c79a2a36
Kang-DS <dsk08208@gmail.com>
2022-02-22 05:37:58 +0900

    baekjoon

--- baekjoon_/src/baekjoon_1/baekjoon_18111.java
+++ baekjoon_/src/baekjoon_1/baekjoon_18111.java
@@ -6,6 +6,22 @@ public class baekjoon_18111 {
 	static StringBuilder sb = new StringBuilder();
 	static int[][] block;
 	static int count=0;
+	static int min=10000;
+	static int sub=0;
+	static int mis(int target,int n1,int n2) {
+		int c = count;
+		for(int i=0;i<n1;i++) {
+			for(int j=0;j<n2;j++) {
+				if(block[i][j]>target) {
+					c+=(block[i][j]-target)*2;
+				}
+				else if(block[i][j]<target) {
+					c+=(target-block[i][j]);
+				}
+			}
+		}
+		return c;
+	}
 	public static void main(String[] args) throws IOException{
 		String str = br.readLine();
 		String[] str2 = str.split(" ");
@@ -13,19 +29,28 @@ public class baekjoon_18111 {
 		int n2 = Integer.parseInt(str2[1]);
 		int n3 = Integer.parseInt(str2[2]);
 		block = new int[n1][n2];
-		int max=0;
-		int min=256;
-		int min_count=0;
-		int max_count=0;
+		int time=0;
+		int high=0;
 		for(int i=0;i<n1;i++) {
 			String s = br.readLine();
-			String[] s2 = str.split(" ");
+			String[] s2 = s.split(" ");
 			for(int j=0;j<n2;j++) {
 				block[i][j] = Integer.parseInt(s2[j]);
-				max = Math.max(max, block[i][j]);
 				min = Math.min(min, block[i][j]);
+				sub+=block[i][j];
 			}
 		}
-		
+		time = (sub+n3)*2;
+		sub=time/(n1*n2)/2;	
+		for(int i=min;i<=sub;i++) {
+			int a = mis(i,n1,n2);
+			if(time>=a) {
+				time = a;
+				high = i;
+			}
+		}
+		sb.append(time+" "+high);
+		bw.append(sb.toString());
+		bw.flush();
 	}
 }
