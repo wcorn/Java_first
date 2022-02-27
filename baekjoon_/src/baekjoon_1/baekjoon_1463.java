e10de74c46b4ad7a8bf73460e96c95e6b2cbac37
Kang-DS <dsk08208@gmail.com>
2022-02-27 22:52:53 +0900

    bb

--- baekjoon_/src/baekjoon_1/baekjoon_1463.java
+++ baekjoon_/src/baekjoon_1/baekjoon_1463.java
@@ -4,19 +4,7 @@ public class baekjoon_1463 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
-<<<<<<< HEAD
 	static int[] a;	
-	static int find(int n) {
-		int count=0;
-		int num=1;
-		int i=0;
-		while(true) {
-			int k = num;
-			a[i] = k;
-			if(a[i]==n) {
-				break;
-			}
-=======
 	static int[] mis;
 	static int i=0;
 	static void mission(int k) {
@@ -32,23 +20,15 @@ public class baekjoon_1463 {
 		int count=0;
 		while(mis[i]!=n) {
 			
->>>>>>> 84b2b8d8be9fe3c194d815fb2e61c6bea5265aaa
 		}
 		return count;
 	}
 	public static void main(String[] args) throws IOException{
 		int n = Integer.parseInt(br.readLine());
-<<<<<<< HEAD
 		a = new int[n+1];
-=======
 		mis = new int[n];
->>>>>>> 84b2b8d8be9fe3c194d815fb2e61c6bea5265aaa
 		sb.append(find(n));
 		bw.write(sb.toString());
 		bw.flush();
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 84b2b8d8be9fe3c194d815fb2e61c6bea5265aaa
