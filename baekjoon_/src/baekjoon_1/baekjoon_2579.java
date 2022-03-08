d459405a3da577ed3d0240b9553ee5ac495a8418
wcorn <dsk08208@gmail.com>
2022-03-08 14:30:58 +0900

    baekjon_2579

--- baekjoon_/src/baekjoon_1/baekjoon_2579.java
+++ baekjoon_/src/baekjoon_1/baekjoon_2579.java
@@ -5,14 +5,27 @@ public class baekjoon_2579 {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
 	static int[] dp;
+	static int[] n;
 	public static void main(String[] args) throws IOException{
 		int num = Integer.parseInt(br.readLine());
-		int[] n = new int[num+1];
+		n = new int[num+1];
 		dp = new int[num+1];
-		int a = num/3;
-		for(int i=0;i<num;i++) {
+		for(int i=1;i<=num;i++) {
 			n[i] = Integer.parseInt(br.readLine());
 		}
+		if(num==1) {
+			sb.append(n[1]);
+			bw.write(sb.toString());
+			bw.flush();
+			return;
+		}
 		dp[1] = n[1];
+		dp[2] = n[1]+n[2];
+		for(int i=3;i<=num;i++) {
+			dp[i] = Math.max(dp[i-3]+n[i-1]+n[i], dp[i-2]+n[i]);
+		}
+		bw.write(dp[num] + "\n");
+		bw.flush();
+		bw.close();
 	}
 }
--- baekjoon_/src/baekjoon_1/baekjoon_2606.java
+++ baekjoon_/src/baekjoon_1/baekjoon_2606.java
@@ -4,39 +4,17 @@ public class baekjoon_2606 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
+	static int[] com;
+	static int[][] con;
 	public static void main(String[] args)throws IOException {
-		int num = Integer.parseInt(br.readLine());
-		int time = Integer.parseInt(br.readLine());
-		int[] n = new int[num+1];
-		int[][] a = new int[num+1][2];
-		int target = 1;
-		int count=0;
-		n[target]++;
-		for(int i=1;i<time;i++) {
+		int com_num = Integer.parseInt(br.readLine());
+		int com_con = Integer.parseInt(br.readLine());
+		com = new int[com_num+1];
+		con = new int[com_num+1][2];
+		for(int i=0;i<com_con;i++) {
 			String str = br.readLine();
 			String[] str2 = str.split(" ");
-			a[i][0] = Integer.parseInt(str2[0]);
-			a[i][1] = Integer.parseInt(str2[1]);
+						
 		}
-		for(int k=0;k<time;k++) {
-			for(int i=1;i<=num;i++) {
-				for(int j=0;j<2;j++) {
-					if(a[i][j]==target) {
-						target = a[i][1-j];
-						n[target]++;
-						break;
-					}
-				}
-			}
-			
-		}
-		for(int i=2;i<=num;i++) {
-			if(n[i]>0) {
-				count++;
-			}
-		}
-		sb.append(count);
-		bw.write(sb.toString());
-		bw.flush();
 	}
 }
