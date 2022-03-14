cc11b1c1a0626d901ed0ab30f681e9ea16566bbe
Kang-DS <dsk08208@gmail.com>
2022-03-14 23:21:28 +0900

    college

---
+++ College/src/Ex1_2a.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ex1_2a {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		System.out.println(2022-year-1);
+		sc.close();
+	}
+}
