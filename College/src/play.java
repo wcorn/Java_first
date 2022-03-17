97dc29ad9234d2cd4d778a7002d7560fe7744647
Kang-DS <dsk08208@gmail.com>
2022-03-17 10:02:44 +0900

    de

---
+++ College/src/play.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class play {
+	public static void main(String[] args) {
+		int a = 40;
+		double b = 8.25;
+		System.out.println(a*b);
+		double c = 1/2+1/2;
+		System.out.println(c);
+	}
+
+}
